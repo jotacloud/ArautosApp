6fa94c6569eda3221d4783178a25811f0a6956f3
João Paulo N. Rodrigues <joaopaulonr.cloud@gmail.com>
2024-05-24 12:17:10 -0300

    feat: added error messages

---
+++ utils/errorMessages.go
@@ -0,0 +1,17 @@
+package utils
+
+const (
+	ErrFailedToReadBody    = "Falha ao Tentar Ler o Corpo da Requisição!"
+	ErrFailedToEncryptPwd  = "Falha ao Tentar Encriptar a Senha!"
+	ErrFailedToCreateUser  = "Falha ao Tentar Criar o Usuário!"
+	SuccessUserRegistered  = "Usuário Cadastrado com Sucesso!"
+	ErrInvalidCredentials  = "Senha ou Email Inválido!"
+	ErrInvalidEmail        = "Email Inválido!"
+	ErrInvalidPassword     = "Senha Inválida!"
+	ErrTokenNotFound       = "Token não encontrado!"
+	ErrFailedToCreateToken = "Erro ao Criar o Token!"
+	ErrInvalidToken        = "Token inválido!"
+	ErrExpiredToken        = "Token expirado!"
+	ErrUserNotFound        = "Usuário não encontrado!"
+	ErrUnexpectedSigning   = "Método de assinatura não identificado!"
+)
